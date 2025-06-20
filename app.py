e3e91d27f00f9b183490fcd6128e8420245cc5a2
unknown <sahasayan100@gmail.com>
2025-06-20 22:23:27 +0530

    changed port to env supportable

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, request
+import os
 
 app = Flask(__name__)
 
@@ -37,4 +38,5 @@ def process():
     return render_template("result.html", message=message, name=name)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
